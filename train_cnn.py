02f6c98a0612ff08dc8d6f5de190ceefdc830919
DUNCAN BOYD <duncan@wapta.ca>
2025-02-24 13:45:25 -0800

    model builds and trains

--- train_cnn.py
+++ train_cnn.py
@@ -1,30 +1,27 @@
 import numpy as np
 from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Lambda
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 from load_data import build_dataset
 
+NUM_FEATURES = 1
+SEQUENCE_LENGTH = 400
+NUM_SAMPLES = 715
+
 def preprocess_data(X, y):
-
-    num_samples = 1000
-    sequence_length = 50
-    num_features = 1
-
-    # Generate random time series data
-    X = np.random.randn(num_samples, sequence_length, num_features)
-    y = np.random.randint(0, 2, size=(num_samples,))  # Binary classification labels
 
     # Data normalization (standardization)
     scaler = StandardScaler()
-    X = X.reshape(-1, num_features)  # Reshaping for fitting scaler
+    X = X.reshape(-1, NUM_FEATURES)  # Reshaping for fitting scaler
     X = scaler.fit_transform(X)  # Standardize each feature
-    X = X.reshape(num_samples, sequence_length, num_features)  # Reshaping back to (num_samples, sequence_length, num_features)
+    X = X.reshape(NUM_SAMPLES, SEQUENCE_LENGTH, NUM_FEATURES)  # Reshaping back to (num_samples, sequence_length, num_features)
 
     return X, y
+
 
 def train_model(X_train, y_train, X_val, y_val):
 
@@ -49,21 +46,26 @@ def train_model(X_train, y_train, X_val, y_val):
     # Compile the model
     model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
+    model.summary()
+
+    model.fit(X_train, y_train, epochs=10, batch_size=32)
 
     return model
 
-def test_model(model):
+def test_model(model, X_test, y_test):
 
     # Evaluate the model on test data
     test_loss, test_acc = model.evaluate(X_test, y_test)
-    print(f"Test accuracy: {test_acc:.4f}")
+    print(f"\n\nFinal test accuracy: {test_acc:.4f}\nFinal test loss: {test_loss:.4f}")
 
     return
 
 if __name__ == "__main__":
 
     X, y = build_dataset()
+
+    assert(NUM_SAMPLES == X.shape[0])
+    assert(SEQUENCE_LENGTH == X.shape[1])
 
     X, y = preprocess_data(X, y)
 
