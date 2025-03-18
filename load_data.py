cd49de8f4035912b9d88f8d73ebb4993f075576b
duncan <duncan@wapta.ca>
2025-03-18 11:26:52 -0700

    formatted python scripts with black

--- load_data.py
+++ load_data.py
@@ -1,7 +1,7 @@
 """
 Loads pulse data from my capstone project and formats it for machine learning.
 
-Duncan Boyd 
+Duncan Boyd
 duncan@wapta.ca
 Feb 24, 2025
 """
@@ -10,25 +10,31 @@ import os
 import h5py
 import numpy as np
 
-DATA_FOLDER = 'data'
+DATA_FOLDER = "data"
 
-def load_data_from_file(file_path, X_data:list, y_data:list):
 
-    with h5py.File(file_path, 'r') as file:
+def load_data_from_file(file_path, X_data: list, y_data: list):
+
+    with h5py.File(file_path, "r") as file:
 
         for series_name in list(file.keys()):
             ppg_data = file[f"{series_name}/ppg_data"][:]
             X_data.append(ppg_data)
 
             pulse_label = file[f"{series_name}/pulse_label"][()].decode()
-            y_data.append(1 if pulse_label == 'T' else 0)
+            y_data.append(1 if pulse_label == "T" else 0)
 
-            #hr_label = file[f"{series_name}/hr_label"][()] # unused for now
+            # hr_label = file[f"{series_name}/hr_label"][()] # unused for now
 
     return X_data, y_data
-    
+
+
 def build_dataset():
-    file_paths = [os.path.join(DATA_FOLDER, file) for file in os.listdir(DATA_FOLDER) if os.path.isfile(os.path.join(DATA_FOLDER, file))]
+    file_paths = [
+        os.path.join(DATA_FOLDER, file)
+        for file in os.listdir(DATA_FOLDER)
+        if os.path.isfile(os.path.join(DATA_FOLDER, file))
+    ]
 
     X_data = []
     y_data = []
@@ -45,7 +51,3 @@ def build_dataset():
 if __name__ == "__main__":
 
     X, y = build_dataset()
-
-
-    
-
--- train_cnn.py
+++ train_cnn.py
@@ -15,7 +15,8 @@ Feb 24, 2025
 """
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # suppress a TF warning about CPU use
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # suppress a TF warning about CPU use
 
 import numpy as np
 import logging
@@ -38,19 +39,23 @@ import csv
 
 from load_data import build_dataset
 
-SPEED_MODE = True # train with a single epoch for debugging
-LOGGING_DIR = 'logging'
-MODELS_DIR = 'models'
-FIGURES_DIR = 'figures'
-RESULTS_DIR = 'results'
+SPEED_MODE = True  # train with a single epoch for debugging
+LOGGING_DIR = "logging"
+MODELS_DIR = "models"
+FIGURES_DIR = "figures"
+RESULTS_DIR = "results"
 SAVE_MODEL = False
 
+
 class ModelConfig:
-    def __init__(self, conv_layers=3, dense_points=128, dropout_rate=0.3, l2_decay=0.01):
+    def __init__(
+        self, conv_layers=3, dense_points=128, dropout_rate=0.3, l2_decay=0.01
+    ):
         self.conv_layers = conv_layers
         self.dense_points = dense_points
         self.dropout_rate = dropout_rate
         self.l2_decay = l2_decay
+
 
 def preprocess_data(X, y):
     """
@@ -66,9 +71,9 @@ def preprocess_data(X, y):
 
         # apply a band pass filter to the data
         nyquist = 0.5 * 60
-        low = 0.5 / nyquist # lowest heartrate of 0.5Hz/30BPM
-        high = 5 / nyquist # highest heartrate of 5Hz/300BPM
-        b, a = signal.butter(1, [low, high], btype='band')
+        low = 0.5 / nyquist  # lowest heartrate of 0.5Hz/30BPM
+        high = 5 / nyquist  # highest heartrate of 5Hz/300BPM
+        b, a = signal.butter(1, [low, high], btype="band")
         ppg_data = signal.filtfilt(b, a, ppg_data)
 
         # remove baseline drift with a polynomial fit
@@ -82,7 +87,7 @@ def preprocess_data(X, y):
     return X, y
 
 
-def train_model(X_train, y_train, X_val, y_val, model_cfg:ModelConfig):
+def train_model(X_train, y_train, X_val, y_val, model_cfg: ModelConfig):
 
     model = Sequential()
 
@@ -91,22 +96,42 @@ def train_model(X_train, y_train, X_val, y_val, model_cfg:ModelConfig):
     # 1D convolutional layers
     filters = 64
     for ii in range(model_cfg.conv_layers):
-        model.add(Conv1D(filters=filters, kernel_size=3, activation='relu'))
+        model.add(Conv1D(filters=filters, kernel_size=3, activation="relu"))
         model.add(MaxPooling1D(pool_size=2))
         filters *= 2
 
     # flatten and dense layers
     model.add(Flatten())
-    model.add(Dense(2*model_cfg.dense_points, activation='relu', kernel_regularizer=l2(model_cfg.l2_decay)))
+    model.add(
+        Dense(
+            2 * model_cfg.dense_points,
+            activation="relu",
+            kernel_regularizer=l2(model_cfg.l2_decay),
+        )
+    )
     model.add(Dropout(model_cfg.dropout_rate))
-    model.add(Dense(model_cfg.dense_points, activation='relu', kernel_regularizer=l2(model_cfg.l2_decay)))
+    model.add(
+        Dense(
+            model_cfg.dense_points,
+            activation="relu",
+            kernel_regularizer=l2(model_cfg.l2_decay),
+        )
+    )
 
-    model.add(Dense(1, activation='sigmoid'))  # output layer for binary classification
+    model.add(Dense(1, activation="sigmoid"))  # output layer for binary classification
 
     # compile and fit the model
-    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
     callback = EarlyStopping(patience=5, restore_best_weights=True)
-    history = model.fit(X_train, y_train, epochs=(1 if SPEED_MODE else 200), batch_size=32, validation_data=(X_val, y_val), callbacks=callback, verbose=0)
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=(1 if SPEED_MODE else 200),
+        batch_size=32,
+        validation_data=(X_val, y_val),
+        callbacks=callback,
+        verbose=0,
+    )
 
     return model, history
 
@@ -115,11 +140,11 @@ def augment_data(X, y):
 
     augmented_X = []
     augmented_y = []
-    
+
     for series, label in zip(X, y):
         augmented_X.append(series)
         augmented_y.append(label)
-        
+
         # randomly scale the data
         scale_factor = np.random.uniform(0.5, 2.0)
         augmented_X.append(series * scale_factor)
@@ -128,10 +153,14 @@ def augment_data(X, y):
         # randomly stretch data along the x axis
         stretch_factor = np.random.uniform(1.0, 2.0)
         x_stretched = np.linspace(0, len(series), int(len(series) * stretch_factor))
-        interpolator = interp1d(list(range(0, len(series))), series, kind='linear', fill_value="extrapolate")
+        interpolator = interp1d(
+            list(range(0, len(series))), series, kind="linear", fill_value="extrapolate"
+        )
         stretched_series = interpolator(x_stretched)
-        offset = random.randint(0, len(stretched_series) - len(series)) # pick a random segment of the stretched section
-        augmented_X.append(stretched_series[offset:offset+X.shape[1]])
+        offset = random.randint(
+            0, len(stretched_series) - len(series)
+        )  # pick a random segment of the stretched section
+        augmented_X.append(stretched_series[offset : offset + X.shape[1]])
         augmented_y.append(label)
 
     augmented_X = np.asarray(augmented_X)
@@ -139,12 +168,15 @@ def augment_data(X, y):
 
     return augmented_X, augmented_y
 
+
 def model_trial(model_cfg, logger):
 
     logger.info(f"1D CNN for Pulse Detection")
 
     X_data, y_data = build_dataset()
-    logger.info(f"Data loaded, {100 * np.count_nonzero(y_data == 1) / np.count_nonzero(y_data == 0):.2f}% of data has a pulse")
+    logger.info(
+        f"Data loaded, {100 * np.count_nonzero(y_data == 1) / np.count_nonzero(y_data == 0):.2f}% of data has a pulse"
+    )
 
     X_data, y_data = preprocess_data(X_data, y_data)
 
@@ -157,15 +189,21 @@ def model_trial(model_cfg, logger):
         X_test = X_data[test_index]
         y_test = y_data[test_index]
 
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2) # split val data
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train, y_train, test_size=0.2
+        )  # split val data
         X_train, y_train = augment_data(X_train, y_train)
-        logger.info(f"Data processed, divided, and augmented: train: {len(X_train)} | test: {len(X_test)} | val: {len(X_val)}")
+        logger.info(
+            f"Data processed, divided, and augmented: train: {len(X_train)} | test: {len(X_test)} | val: {len(X_val)}"
+        )
 
         model, history = train_model(X_train, y_train, X_val, y_val, model_cfg)
         logger.info(f"Model trained for {len(history.epoch)} epochs")
 
         test_loss, test_acc = model.evaluate(X_test, y_test, verbose=0)
-        logger.info(f"Test accuracy is {100*test_acc:.2f}% and final test loss is {test_loss:.4f}")
+        logger.info(
+            f"Test accuracy is {100*test_acc:.2f}% and final test loss is {test_loss:.4f}"
+        )
 
         model_accuracies.append(test_acc)
 
@@ -173,13 +211,17 @@ def model_trial(model_cfg, logger):
     logger.info(f"Mean accuracy is {100*mean_acc:.2f}%")
 
     if SAVE_MODEL:
-        model_filename = os.path.join(MODELS_DIR, f'model_{int(100*test_acc)}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.keras')
+        model_filename = os.path.join(
+            MODELS_DIR,
+            f'model_{int(100*test_acc)}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.keras',
+        )
         model.save(model_filename)
         logging.info(f"Model saved as: {model_filename}")
 
     logger.info(f"Done!\n\n")
 
     return mean_acc
+
 
 def sweep_param(param, sweep, logger, csv_path):
 
@@ -189,13 +231,13 @@ def sweep_param(param, sweep, logger, csv_path):
     sweep_accs = {}
     for val in sweep:
 
-        if param == 'conv_layers':
+        if param == "conv_layers":
             model_cfg.conv_layers = val
-        elif param == 'dense_points':
+        elif param == "dense_points":
             model_cfg.dense_points = val
-        elif param == 'dropout_rate':
+        elif param == "dropout_rate":
             model_cfg.dropout_rate = val
-        elif param == 'l2_rate':
+        elif param == "l2_rate":
             model_cfg.l2_decay = val
 
         logging.info(f"Trying {val}")
@@ -210,11 +252,19 @@ def sweep_param(param, sweep, logger, csv_path):
             break
 
         sweep_accs[val] = mean_acc
-        
-        with open(csv_path, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([datetime.now(), model_cfg.conv_layers, model_cfg.dense_points, model_cfg.dropout_rate, model_cfg.l2_decay, mean_acc])
 
+        with open(csv_path, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [
+                    datetime.now(),
+                    model_cfg.conv_layers,
+                    model_cfg.dense_points,
+                    model_cfg.dropout_rate,
+                    model_cfg.l2_decay,
+                    mean_acc,
+                ]
+            )
 
     logging.info(f"{param} Sweep Results:\n{sweep_accs}\n\n")
 
@@ -225,33 +275,49 @@ def sweep_param(param, sweep, logger, csv_path):
 
     return
 
+
 if __name__ == "__main__":
 
-    log_filename = os.path.join(LOGGING_DIR, f'log_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log')
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler(log_filename), logging.StreamHandler()])
+    log_filename = os.path.join(
+        LOGGING_DIR, f'log_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'
+    )
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[logging.FileHandler(log_filename), logging.StreamHandler()],
+    )
     logger = logging.getLogger()
 
     model_cfg = ModelConfig()
 
     # configure sweeps
-    conv_sweep = list(range(1, 6+1)) # 1 - 6, 1 increments
-    dense_sweep = [32 * (2 ** i) for i in range(6)] # 32 - 1024, power of 2 increments
+    conv_sweep = list(range(1, 6 + 1))  # 1 - 6, 1 increments
+    dense_sweep = [32 * (2**i) for i in range(6)]  # 32 - 1024, power of 2 increments
     dropout_sweep = list(np.arange(0.2, 0.500001, 0.05))
-    dropout_sweep = np.round(dropout_sweep, 2) # 0.2 - 0.5, 0.05 increments
+    dropout_sweep = np.round(dropout_sweep, 2)  # 0.2 - 0.5, 0.05 increments
     l2_sweep = list(np.arange(0, 0.100001, 0.01))
-    l2_sweep = np.round(l2_sweep, 2) # 0 - 0.1, 0.01 increments
+    l2_sweep = np.round(l2_sweep, 2)  # 0 - 0.1, 0.01 increments
 
-    headers = ['Time', 'Conv Layers', 'Dense_layers', 'Dropout Rate', 'L2 Rate', 'Accuracy']
+    headers = [
+        "Time",
+        "Conv Layers",
+        "Dense_layers",
+        "Dropout Rate",
+        "L2 Rate",
+        "Accuracy",
+    ]
 
-    csv_path = os.path.join(RESULTS_DIR, f'results_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv')
+    csv_path = os.path.join(
+        RESULTS_DIR, f'results_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
+    )
     if not os.path.exists(csv_path):
-        with open(csv_path, mode='w', newline='') as file:
+        with open(csv_path, mode="w", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(headers)
 
-    sweep_param('conv_layers', conv_sweep, logger, csv_path)
-    sweep_param('dense_points', dense_sweep, logger, csv_path)
-    sweep_param('dropout_rate', dropout_sweep, logger, csv_path)
-    sweep_param('l2_rate', l2_sweep, logger, csv_path)
+    sweep_param("conv_layers", conv_sweep, logger, csv_path)
+    sweep_param("dense_points", dense_sweep, logger, csv_path)
+    sweep_param("dropout_rate", dropout_sweep, logger, csv_path)
+    sweep_param("l2_rate", l2_sweep, logger, csv_path)
 
     logger.info(f"Extra Done!\n\n")
